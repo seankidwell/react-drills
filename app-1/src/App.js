013ce8fb2c960eb276aa26ec20fbc033690c80e6
Sean Kidwell <seankidwell@Seans-MacBook-Pro.local>
2018-06-09 22:10:42 -0600

    done

--- app-1/src/App.js
+++ app-1/src/App.js
@@ -3,16 +3,20 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      message: ""
+    }
+  }
+  changer(val) {
+    this.setState({message: val})
+  }
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+          <input onChange={e => this.changer(e.target.value)} type="text"/>
+          <p>{this.state.message}</p>
       </div>
     );
   }
