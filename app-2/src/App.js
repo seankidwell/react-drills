68196845bfd9ca6b703d55e4067612213ea28361
Sean Kidwell <seankidwell@Seans-MacBook-Pro.local>
2018-06-10 21:14:28 -0600

    done

--- app-2/src/App.js
+++ app-2/src/App.js
@@ -3,16 +3,26 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      array: ["pizza", "spaghetti", "ice cream", "sushi", "hotdog"],
+      userInput: ''
+    }
+  }
+  changer(val) {
+    this.setState({userInput: val})
+  }
   render() {
+    let foodsToDisplay = this.state.array.filter((e,i) => {
+      return e.includes(this.state.userInput);
+    }).map((e,i) => {
+      return <h2 key={i}>{e}</h2>
+    })
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <input onChange={e => this.changer(e.target.value)} type="test" />
+        {foodsToDisplay}
       </div>
     );
   }
